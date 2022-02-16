95489b758fd276ad60e0442cd41a801471107a09
Anny Pereira <isabelle.pereira04@gmail.com>
2022-02-15 23:38:15 -0300

    Teste Backend com Erros

--- Projetos/2022_3T_ProjetoBase_Backend/Patrimonio.Text/Controllers/EquipamentoController.cs
+++ Projetos/2022_3T_ProjetoBase_Backend/Patrimonio.Text/Controllers/EquipamentoController.cs
@@ -1,6 +1,10 @@
-﻿using Moq;
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Patrimonio.Controllers;
 using Patrimonio.Domains;
 using Patrimonio.Interfaces;
+using System;
+using System.Collections.Generic;
 using Xunit;
 
 namespace Patrimonio.Text.Controllers
@@ -10,8 +14,49 @@ namespace Patrimonio.Text.Controllers
         [Fact]
         public void DeveListarTodosEquipamentos()
         {
-            var fakeRepository = new Mock<IEquipamentoRepository>();
+            var listaEquipamentos = new List<Equipamento>();
 
+            var equipamento1 = new Equipamento();
+            equipamento1.Id = 1;
+            equipamento1.NomePatrimonio = "Impressora";
+            equipamento1.Imagem = "teste";
+            equipamento1.Descricao = "Imprime colorido e preto e branco.";
+            equipamento1.Ativo = true;
+            equipamento1.DataCadastro = DateTime.Now;
+
+            listaEquipamentos.Add(equipamento1);
+
+            var fakeRepository = new Mock<IEquipamentoRepository>();
+            fakeRepository.Setup(x => x.Listar()).Returns(listaEquipamentos);
+
+            var controller = new EquipamentosController(fakeRepository.Object);
+
+            var resultado = controller.Listar();
+
+            Assert.IsType<ObjectResult>(resultado);
+
+        }
+
+        [Fact]
+        public void DeveAtualizarUmEquipamento()
+        {
+
+            var equipamento1 = new Equipamento();
+            equipamento1.Id = 1;
+            equipamento1.NomePatrimonio = "Impressora";
+            equipamento1.Imagem = "teste";
+            equipamento1.Descricao = "Imprime colorido e preto e branco.";
+            equipamento1.Ativo = true;
+            equipamento1.DataCadastro = DateTime.Now;
+
+            var fakeRepository = new Mock<IEquipamentoRepository>();
+            fakeRepository.Setup(x => x.Alterar(equipamento1));
+
+            var controller = new EquipamentosController(fakeRepository.Object);
+
+            var resultado = controller.Alterar(equipamento1);
+
+            Assert.IsType<ObjectResult>(resultado);
 
         }
     }
